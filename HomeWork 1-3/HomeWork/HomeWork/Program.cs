34d6ee01688d8ea6b6021cbfdb1274d927bfb246
MaxPostoienko <71272305+MaxPostoienko@users.noreply.github.com>
2023-09-06 16:00:47 +0200

    HomeWork 1-3

---
+++ HomeWork 1-3/HomeWork/HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿
+    // Hello + Name!
+    Console.WriteLine("Whats your name");
+    string? name = Console.ReadLine();
+    Console.WriteLine($"Hello {name}!");
+
+    // Reverse numbers
+    int x, y;
+    Console.WriteLine("Enter the first number");
+    x = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter the second number");
+    y = int.Parse(Console.ReadLine());
+    Console.WriteLine($"x={x} y={y}");
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    Console.WriteLine($"Swap x={x} y={y}");
+
+    // Lenght numbers
+    Console.WriteLine("Enter number");
+    int Number = int.Parse(Console.ReadLine());
+    Console.WriteLine(Number.ToString().Length);
